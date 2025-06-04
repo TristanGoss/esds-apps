35740768d350f1de8c1948bd59e82f1e2681652d
Tristan Goss <tristanjdgoss@gmail.com>
2025-06-04 16:43:37 +0100

    tweaking styling around rapid card scanner

--- public/js/card_scanner.js
+++ public/js/card_scanner.js
@@ -1,25 +1,56 @@
 
 const allDiv = document.getElementById("all");
 const resultDiv = document.getElementById("result");
-const scanner = new Html5Qrcode("reader");
+const scanner = new Html5Qrcode("qr-reader");
 const sounds = {
   valid: new Audio("/public/330047__paulmorek__beep-04-positive-2.mp3"),
   invalid: new Audio("/public/330049__paulmorek__beep-04-negative.mp3")
 };
+let isScannerRunning = false;
 let readyForScan = true;
 let lastDecodedText = null;
-let isValidSoundLoaded = false;
-let isInvalidSoundLoaded = false;
 
-// Force browsers to preload
-sounds.valid.load();
-sounds.invalid.load();
-
-// Force iOS browsers to preload
+// Force the user to click once to enable the scanner.
+// This works around the issue where iOS won't allow
+// audio to play until the user has interacted with the page.
 document.addEventListener("click", () => {
-    sounds.valid.play().catch(() => {});
-    sounds.invalid.play().catch(() => {});
-  }, { once: true });
+  if (isScannerRunning) return;
+
+  // buffer the audio so that it plays correctly on the first scan
+  [sounds.valid, sounds.invalid].forEach(sound => {
+    sound.volume = 0;
+    sound.play().then(() => {
+      sound.pause();
+      sound.currentTime = 0;
+      sound.volume = 1;
+    }).catch(() => {});
+  });
+
+  // start the scanner
+  scanner.start(
+    { facingMode: "environment" },
+    {
+      fps: 10,
+      qrbox: { width: 1000, height: 1000 }
+    },
+    (decodedText, decodedResult) => {
+      if (!readyForScan || decodedText === lastDecodedText) return;
+      readyForScan = false; // prevent repeated scans until no QR code is detected.
+      lastDecodedText = decodedText
+      validateCard(decodedText).then(() => {
+          setTimeout(() => {lastDecodedText = null}, 4000) // debounce repeated scans of the same code
+      });
+    },
+    (errorMessage) => {
+      // If nothing is detected (i.e. the code has been removed from view) we may ready for another scan
+      readyForScan = true;
+    }
+  ).then(() => {
+      // Hide the help text once scanner is running
+      document.getElementById("qr-help").classList.add("hidden");
+  isScannerRunning = true;
+  })
+}, { once: true });
 
 function showResult(message, isValid) {
   resultDiv.textContent = message;
@@ -44,46 +75,5 @@ async function validateCard(data) {
     showResult(valid ? "✅ CARD VALID" : "❌ CARD INVALID", valid);
   } catch (err) {
     showResult("❌ FAILED TO VALIDATE", false);
-  }
-}
-
-// once sounds have loaded, enable scanner
-sounds.valid.addEventListener("canplaythrough", () => {
-  isValidSoundLoaded = true;
-  console.log("DEBUG A");
-  enableScannerIfReady();
-})
-
-sounds.invalid.addEventListener("canplaythrough", () => {
-  isInvalidSoundLoaded = true;
-  console.log("DEBUG B");
-  enableScannerIfReady();
-})
-
-// we don't enable the scanner until both sounds are loaded.
-// This prevents immediate scans (when the page is refreshed while pointing at a PR code)
-// from failing to sound, and hints to the user that refreshing the page all the time isn't a good idea.
-function enableScannerIfReady() {
-  console.log("enableScannerIfReady called");
-  if (isInvalidSoundLoaded && isValidSoundLoaded) {
-    scanner.start(
-      { facingMode: "environment" },
-      {
-        fps: 10,
-        qrbox: { width: 1000, height: 1000 }
-      },
-      (decodedText, decodedResult) => {
-        if (!readyForScan || decodedText === lastDecodedText) return;
-        readyForScan = false; // prevent repeated scans until no QR code is detected.
-        lastDecodedText = decodedText
-        validateCard(decodedText).then(() => {
-            setTimeout(() => {lastDecodedText = null}, 4000) // debounce repeated scans of the same code
-        });
-      },
-      (errorMessage) => {
-        // If nothing is detected (i.e. the code has been removed from view) we may ready for another scan
-        readyForScan = true;
-      }
-    );
   }
 }
